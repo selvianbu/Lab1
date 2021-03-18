654288815faa719fc96ff9870bf131f47e6ac240
Anbu Selvi <nitthiyah19anbu@gmail.com>
2021-03-18 23:22:26 +0800

    Second Commit

---
+++ samples/src/samples/demo/location.java
@@ -0,0 +1,15 @@
+package samples.demo;
+
+public class location {
+	
+	private String location;
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+}

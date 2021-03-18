84c97b00817416050cd1a4e349c9df4be98a6f85
Anbu Selvi <nitthiyah19anbu@gmail.com>
2021-03-18 23:24:31 +0800

    added more attributes

--- samples/src/samples/demo/location.java
+++ samples/src/samples/demo/location.java
@@ -3,6 +3,8 @@ package samples.demo;
 public class location {
 	
 	private String location;
+	private double distance;
+	private String name;
 
 	public String getLocation() {
 		return location;
@@ -10,6 +12,22 @@ public class location {
 
 	public void setLocation(String location) {
 		this.location = location;
+	}
+
+	public double getDistance() {
+		return distance;
+	}
+
+	public void setDistance(double distance) {
+		this.distance = distance;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 }
